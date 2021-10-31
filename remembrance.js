9ecaa46fa8fa842035583a89e60989d6eadadc5c
Ravi Chandrasekaran <ravichandrasekaran@gmail.com>
2021-10-31 11:37:16 -0700

    Working annotations

--- remembrance.js
+++ remembrance.js
@@ -18,38 +18,15 @@ function getDataPromise() {
   return dat;
 }
 
-function getDataOriginal() {
-  // Create data
-  var parseTime = d3.timeParse("%Y-%m-%d");
-  var data = [
-    {
-      name: "Papi Edwards",
-      age: 20,
-      race: "Black",
-      gender_identity: "Transgender woman",
-      city: "Louisville",
-      state: "KY",
-      x: 80,
-      date_of_death: parseTime("2019-01-09"),
-      manner: "homocide",
-      misgendered: true
-    },
-    {
-      name: "Penny Proud", age: 21, race: "Black",
-      gender_identity: "Transgender woman", city: "New Orleans",
-      x: 80,
-      date_of_death: parseTime("2019-02-10"),
-      manner: "homocide", misgendered: true
-    },
-    {
-      name: "Test 3", age: 21, race: "Black",
-      gender_identity: "Transgender woman", city: "New Orleans",
-      x: 80,
-      date_of_death: parseTime("2018-02-10"),
-      manner: "homocide", misgendered: true
-    },
-  ];
-  return data;
+function updateAnnotation(annotation) {
+  var element = document.getElementById("description");
+  element.innerHTML = annotation.data.name;
+
+}
+
+function clearAnnotation() {
+  var element = document.getElementById("description");
+  element.innerHTML = "";
 }
 
 function addPanel(gra) {
@@ -162,6 +139,95 @@ function addAnnotations(gra, annotations) {
 
 }
 
+
+function yValues(svg, data, {
+  value = d => d, // convience alias for x
+  label, // convenience alias for xLabel
+  type = d3.scaleLinear,
+  domain, // convenience alias for xDomain
+  x = value, // given d in data, returns the quantitative x value
+  title = null, // given d in data, returns the title
+  radius = 3, // (fixed) radius of the circles
+  padding = 1.5, // (fixed) padding between the circles
+  marginTop = 10, // top margin, in pixels
+  marginRight = 20, // right margin, in pixels
+  marginBottom = 30, // bottom margin, in pixels
+  marginLeft = 20, // left margin, in pixels
+  width = 640, // outer width, in pixels
+  height, // outer height, in pixels
+  xType = type,
+  xLabel = label, // a label for the x-axis
+  xDomain = domain, // [xmin, xmax]
+  xRange = [marginLeft, width - marginRight] // [left, right]
+} = {}) {
+  // Compute values.
+  const X = d3.map(data, x);
+  const T = title == null ? null : d3.map(data, title);
+
+  // Compute which data points are considered defined.
+  const I = d3.range(X.length).filter(i => !isNaN(X[i]));
+
+  // Compute default domains.
+  if (xDomain === undefined) xDomain = d3.extent(X);
+
+  // Construct scales and axes.
+  const xScale = xType(xDomain, xRange).nice();
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+
+  // Compute the y-positions.
+  const Y = dodge(I.map(i => xScale(X[i])), radius * 2 + padding);
+  // Compute the default height;
+  if (height === undefined) height = (d3.max(Y, Math.abs) + radius + padding) * 2 + marginTop + marginBottom;
+
+  // Given an array of x-values and a separation radius, returns an array of y-values.
+  function dodge(X, radius) {
+    const Y = new Float64Array(X.length);
+    const radius2 = radius ** 2;
+    const epsilon = 1e-3;
+    let head = null, tail = null;
+
+    // Returns true if circle ⟨x,y⟩ intersects with any circle in the queue.
+    function intersects(x, y) {
+      let a = head;
+      while (a) {
+        const ai = a.index;
+        if (radius2 - epsilon > (X[ai] - x) ** 2 + (Y[ai] - y) ** 2) return true;
+        a = a.next;
+      }
+      return false;
+    }
+
+    // Place each circle sequentially.
+    for (const bi of d3.range(X.length).sort((i, j) => X[i] - X[j])) {
+
+      // Remove circles from the queue that can’t intersect the new circle b.
+      while (head && X[head.index] < X[bi] - radius2) head = head.next;
+
+      // Choose the minimum non-intersecting tangent.
+      if (intersects(X[bi], Y[bi] = 0)) {
+        let a = head;
+        Y[bi] = Infinity;
+        do {
+          const ai = a.index;
+          let y1 = Y[ai] + Math.sqrt(radius2 - (X[ai] - X[bi]) ** 2);
+          let y2 = Y[ai] - Math.sqrt(radius2 - (X[ai] - X[bi]) ** 2);
+          if (Math.abs(y1) < Math.abs(Y[bi]) && !intersects(X[bi], y1)) Y[bi] = y1;
+          if (Math.abs(y2) < Math.abs(Y[bi]) && !intersects(X[bi], y2)) Y[bi] = y2;
+          a = a.next;
+        } while (a);
+      }
+
+      // Add b to the queue.
+      const b = { index: bi, next: null };
+      if (head === null) head = tail = b;
+      else tail = tail.next = b;
+    }
+
+    return Y;
+  }
+  return Y;
+}
+
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
 // https://observablehq.com/@d3/mirrored-beeswarm
@@ -201,7 +267,6 @@ function MirroredBeeswarmChart(svg, data, {
 
   // Compute the y-positions.
   const Y = dodge(I.map(i => xScale(X[i])), radius * 2 + padding);
-
   // Compute the default height;
   if (height === undefined) height = (d3.max(Y, Math.abs) + radius + padding) * 2 + marginTop + marginBottom;
 
@@ -269,7 +334,57 @@ function MirroredBeeswarmChart(svg, data, {
     .join("circle")
     .attr("cx", i => xScale(X[i]))
     .attr("cy", i => (marginTop + height - marginBottom) / 2 + Y[i])
-    .attr("r", radius);
+    .attr("r", radius / 4);
+
+
+  svg.append('g').selectAll('.myPoint')
+    .data(I)
+    .enter()
+    .append('svg:image')
+    .attr("xlink:href", "assets/rose.svg")
+    .attr("x", i => xScale(X[i]) - radius)
+    .attr("y", i => (marginTop + height - marginBottom) / 2 + Y[i] - radius)
+    .attr("width", radius * 2)
+    .attr("height", radius * 2)
+    .attr("fill", "#FFFFFF");
+
+  data.forEach(function (val, idx, arr) {
+    arr[idx].y = (marginTop + height - marginBottom) / 2 + Y[idx];
+    arr[idx].x = xScale(X[idx]);
+    arr[idx].subject = { radius: radius, radiusPadding: radius / 2 };
+    arr[idx].note = { label: val.name };
+    arr[idx].dy = (Y[idx] > 0 ? 150 : -150);
+    arr[idx].dx = (xScale(X[idx]) > .75 * d3.max(xRange) ? -150 : 150);
+    arr[idx].data = val;
+  });
+  console.log(data);
+
+  const makeAnnotations = d3.annotation()
+    .type(d3.annotationCalloutCircle)
+    .accessors({
+      x: d => d.x,
+      y: d => d.y,
+    })
+    .on('subjectover', function (annotation) {
+      annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+        .classed("hidden", false)
+      updateAnnotation(annotation);
+
+    })
+    .on('subjectout', function (annotation) {
+      annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+        .classed("hidden", true)
+      clearAnnotation();
+    })
+    .annotations(data)
+    ;
+
+  svg.append("g")
+    .call(makeAnnotations);
+
+  svg.selectAll("g.annotation-connector, g.annotation-note")
+    .classed("hidden", true)
+
 
   if (T) dot.append("title")
     .text(i => T[i]);
@@ -298,10 +413,28 @@ var scatter = d3.select("#scatter_area")
 
 var remembrances = getDataPromise();
 remembrances.then(function (data) {
-  MirroredBeeswarmChart(scatter, data, {
-    width: 2000,
+  const y_values = yValues(scatter, data, {
+    width: plot_width,
     height: 1000,
     radius: 30,
+    marginLeft: 50,
+    marginRight: 50,
+    value: d => d.date_of_death,
+    label: "Year",
+    type: d3.scaleTime,
+    // domain: [Date.now() - , Date.now() - 5 * 365.24 * 24 * 60 * 60 * 1000],
+    // xRange: [0, plot_height]
+  });
+  data.forEach(function (val, idx, arr) {
+    arr[idx].y = y_values[idx];
+  });
+  // data.y = yValues(scatter, data);
+  MirroredBeeswarmChart(scatter, data, {
+    width: plot_width,
+    height: 1000,
+    radius: 30,
+    marginLeft: 50,
+    marginRight: 50,
     value: d => d.date_of_death,
     label: "Year",
     type: d3.scaleTime,
